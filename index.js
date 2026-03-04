563c2126f0582b9cb9085e0fb9ff5649d63432d9
moiznasir53 <moiz.nasir@attalimiyahoffice.com>
2026-03-04 16:18:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let obliterateParagraph = document.getElementById("obliterated")
+
+function obliterate() {
+    obliterateParagraph.textContent = "Thank you for destroying Israel, you have obliterated it. You are a true hero."
+    console.log("button clicked")
+}
